f2232681b836cfec000e09063839f1524827e329
minglei23 <minglei.23@hotmail.com>
2023-05-22 02:26:08 -0300

    approve

--- src/service/contractService.ts
+++ src/service/contractService.ts
@@ -25,14 +25,50 @@ const balanceOf = async () => {
         const metaflareContract = getMetaflareContract(signer);
         const address = await signer.getAddress();
         const balance = await metaflareContract.balanceOf(address);
-        console.log('Balance: ', balance);
-        return balance;
+        const balanceNumber = ethers.utils.formatUnits(balance, 18);
+        console.log('Balance: ', balanceNumber);
+        return parseFloat(balanceNumber);
     } catch (error) {
         console.error('BalanceOf Error: ', error);
         throw error;
     }
 };
 
+const approve = async (amount) => {
+    try {
+        const balanceNumber = await balanceOf();
+        if (balanceNumber < amount) {
+            throw new Error('Insufficient balance');
+        }
+        const signer = await getSigner();
+        const metaflareContract = getMetaflareContract(signer);
+        const spender = StakingContractAddr;
+        const tx = await metaflareContract.approve(spender, ethers.utils.parseUnits(amount.toString(), 'wei'));
+        console.log('Approve: ', tx);
+        await tx.wait();
+        console.log('Approve Completed.');
+    } catch (error) {
+        console.error('Approve Error: ', error);
+        throw error;
+    }
+};
+
+const allowance = async () => {
+    try {
+        const signer = await getSigner();
+        const metaflareContract = getMetaflareContract(signer);
+        const address = await signer.getAddress();
+        const spender = StakingContractAddr;
+        const allowance = await metaflareContract.allowance(address, spender);
+        const allowanceNumber = ethers.utils.formatUnits(allowance, 18);
+        console.log('Allowance: ', allowanceNumber);
+        return parseFloat(allowanceNumber);
+    } catch (error) {
+        console.error('Allowance Error: ', error);
+        throw error;
+    }
+};
+
 export const ContractService = {
-    balanceOf,
+    balanceOf, approve, allowance
 };
