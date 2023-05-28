673cfe1577ddecc43b2b81293f9f84920d47a9cc
minglei.23 <minglei.23@hotmail.com>
2023-05-28 07:30:15 -0300

    fix

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -3,7 +3,7 @@ import { Text } from '@chakra-ui/react';
 import { ContractService } from '../service/contractService';
 
 export const Address = () => {
-    const [address, setAddress] = useState(0);
+    const [address, setAddress] = useState("");
 
     useEffect(() => {
         const fetchAddress = async () => {
